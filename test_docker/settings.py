da57c0088f2c3e3498eb68ca021a87bc86392231
annaberezko <site.vizit@gmail.com>
2023-03-09 14:36:05 +0200

    change teste.yml

--- test_docker/settings.py
+++ test_docker/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': config('POSTGRES_DB', ''),
         'USER': config('POSTGRES_USER', ''),
         'PASSWORD': config('POSTGRES_PASSWORD', ''),
-        'HOST': config('POSTGRES_HOST', 'db'),
+        'HOST': config('POSTGRES_HOST', '127.0.0.1'),
         'PORT': config('POSTGRES_PORT', 5432)
     }
 }
