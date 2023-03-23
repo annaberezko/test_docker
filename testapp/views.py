ba2b3875893e5c8e5e5dfd43de32b08a38a12941
annaberezko <site.vizit@gmail.com>
2023-03-23 18:40:26 +0200

    fix bug

--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView
 
 from testapp.models import Book
+# from testapp.tasks import add
 
 
 class BooksListView(ListView):
@@ -9,4 +10,7 @@ class BooksListView(ListView):
     context_object_name = 'books'
 
     def get_queryset(self):
+        # print("Hello, queryset")
+        # temp = add.delay(1, 2)
+        # print(temp)
         return Book.objects.all()
