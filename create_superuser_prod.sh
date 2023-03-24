5af3e3b866750c95dfbe6691850b139edcfe4ea5
annaberezko <site.vizit@gmail.com>
2023-03-24 08:50:48 +0200

    fix bug

--- create_superuser_prod.sh
+++ create_superuser_prod.sh
@@ -1,1 +1,1 @@
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('django','django@admin.com','admin')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser($PROD_USER,$PROD_USER_EMAIL,$PROD_USER_PASS)" | python manage.py shell
